6173e4b320f5f45c13312a2ca624ff7a24aab61e
demin6632-del <demin6632@gmail.com>
2026-09-12 11:01:55 +0500

    Add WebView launcher activity

---
+++ app/src/main/java/com/pokoritel/bashni/MainActivity.java
@@ -0,0 +1,19 @@
+package com.pokoritel.bashni;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        web.loadUrl("file:///android_asset/game.html");
+        setContentView(web);
+    }
+}
